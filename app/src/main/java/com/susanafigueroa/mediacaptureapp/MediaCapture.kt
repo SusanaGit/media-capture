ee0258cae7e3f0f253fa757c333490080d2825a7
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-07 17:01:48 +0100

    implement navigation and MediaCaptureBottomBar

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/MediaCapture.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/MediaCapture.kt
@@ -1,16 +1,32 @@
 package com.susanafigueroa.mediacaptureapp
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentComposer
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.susanafigueroa.mediacaptureapp.ui.CameraScreen
+import com.susanafigueroa.mediacaptureapp.ui.GalleryScreen
+import com.susanafigueroa.mediacaptureapp.ui.PlayerScreen
+
 
 enum class MediaCaptureScreen(@StringRes val title: Int) {
     Gallery(title = R.string.gallery),
@@ -32,12 +48,96 @@ fun MediaCaptureAppBar(
         modifier = modifier,
     )
 }
+
 @Composable
 fun MediaCaptureApp(
+    navController: NavHostController = rememberNavController()
 ) {
+
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = MediaCaptureScreen.valueOf(
+        backStackEntry?.destination?.route ?: MediaCaptureScreen.Gallery.name
+    )
+
     Scaffold(
         topBar = {
-            MediaCaptureAppBar( currentScreen = currentScreen) }
+            MediaCaptureAppBar( currentScreen = currentScreen)
+        },
+        bottomBar = {
+            MediaCaptureBottomBar(
+                navController = navController,
+                currentScreen = currentScreen
+            )
+        }
     ) { innerPadding ->
+
+        NavHost(
+            navController = navController,
+            startDestination = MediaCaptureScreen.Gallery.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+            composable(route = MediaCaptureScreen.Gallery.name) {
+                GalleryScreen()
+            }
+
+            composable(route = MediaCaptureScreen.Camera.name) {
+                CameraScreen()
+            }
+
+            composable(route = MediaCaptureScreen.Player.name) {
+                PlayerScreen()
+            }
+        }
+
+    }
+}
+
+@Composable
+fun MediaCaptureBottomBar(
+    navController: NavHostController,
+    currentScreen: MediaCaptureScreen
+) {
+
+    NavigationBar {
+
+        NavigationBarItem(
+            selected = currentScreen == MediaCaptureScreen.Camera,
+            onClick = {
+                navController.navigate(MediaCaptureScreen.Camera.name)
+            },
+            icon = {
+                Icon(
+                    Icons.Default.Face,
+                    contentDescription = R.string.camera.toString()
+                )
+            }
+        )
+
+        NavigationBarItem(
+            selected = currentScreen == MediaCaptureScreen.Gallery,
+            onClick = {
+                  navController.navigate(MediaCaptureScreen.Gallery.name)
+            },
+            icon = {
+                Icon(
+                    Icons.Default.Face,
+                    contentDescription = R.string.gallery.toString()
+                )
+            }
+        )
+
+        NavigationBarItem(
+            selected = currentScreen == MediaCaptureScreen.Player,
+            onClick = {
+                navController.navigate(MediaCaptureScreen.Player.name)
+            },
+            icon = {
+                Icon(
+                    Icons.Default.Face,
+                    contentDescription = R.string.player.toString()
+                )
+            }
+        )
     }
 }
