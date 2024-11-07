3066760a08a5eca81511d58d72e88a01a79728ce
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-07 20:26:26 +0100

    implement PermissionHandler Composable

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/MainActivity.kt
@@ -1,19 +1,75 @@
 package com.susanafigueroa.mediacaptureapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.susanafigueroa.mediacaptureapp.ui.theme.MediaCaptureAppTheme
 
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MediaCaptureAppTheme {
-                MediaCaptureApp()
+                PermissionHandler()
             }
         }
     }
+
 }
+
+@Composable
+fun PermissionHandler() {
+    val context = LocalContext.current
+
+    val requiredPermissions = listOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    fun checkValuePermissions(): Boolean {
+        return requiredPermissions.all {permission ->
+            ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    var allPermissionsTrue by remember { mutableStateOf(checkValuePermissions()) }
+
+    val launcherForPermissions = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            allPermissionsTrue = permissions.values.all { it }
+    }
+
+    LaunchedEffect(Unit) {
+        if(!allPermissionsTrue) {
+            launcherForPermissions.launch(requiredPermissions.toTypedArray())
+        }
+    }
+
+    if (allPermissionsTrue) {
+        MediaCaptureApp()
+    } else {
+        Text(text = stringResource(R.string.accept_the_permissions_to_continue), modifier = Modifier.padding(16.dp))
+    }
+}
+
--- app/src/main/java/com/susanafigueroa/mediacaptureapp/MediaCapture.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/MediaCapture.kt
@@ -2,9 +2,6 @@ package com.susanafigueroa.mediacaptureapp
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
