3e77a1f962d775c925767472771642527f39f82d
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-07 23:08:04 +0100

    add AndroidView to integrate CameraX PreviewView in Compose

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Camera.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Camera.kt
@@ -14,12 +14,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.susanafigueroa.mediacaptureapp.R
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -27,6 +31,12 @@ import kotlin.coroutines.suspendCoroutine
 @Composable
 fun CameraScreen(
 ) {
+
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val coroutineScope = rememberCoroutineScope()
+    
+    AndroidView(factory = )
 
     Box(
         modifier = Modifier
