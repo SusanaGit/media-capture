c6d39b63b6b5456862321cf41dfc9eed27878c31
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-07 21:11:05 +0100

    obtain an instance of ProcessCameraProvider

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Camera.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Camera.kt
@@ -1,14 +1,29 @@
 package com.susanafigueroa.mediacaptureapp.ui
 
+import android.content.Context
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.core.content.ContextCompat
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 @Composable
 fun CameraScreen(
 ) {
     Column(
     ) {
-        Text(text = "hola camera")
+
     }
 }
+
+suspend fun Context.getCameraProvider(): ProcessCameraProvider =
+    suspendCoroutine { continuation ->
+        ProcessCameraProvider.getInstance(this).also {cameraProvider ->
+            cameraProvider.addListener({
+                continuation.resume(cameraProvider.get())
+            },
+            ContextCompat.getMainExecutor(this))
+        }
+    }
