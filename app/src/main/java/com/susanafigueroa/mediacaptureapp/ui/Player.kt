8030b90fe0dfbc2f3e8a71e8ee990d5d9863d1b0
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-07 16:06:10 +0100

    create screens

---
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Player.kt
@@ -0,0 +1,14 @@
+package com.susanafigueroa.mediacaptureapp.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PlayerScreen(
+) {
+    Column(
+    ) {
+        Text(text = "hola player")
+    }
+}
