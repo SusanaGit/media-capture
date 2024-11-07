464b799ff331d854505bcf575c7686c098d7b734
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-07 22:16:29 +0100

    obtain a ProcessCameraProvider instance to configure the camera

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Camera.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Camera.kt
@@ -1,7 +1,11 @@
 package com.susanafigueroa.mediacaptureapp.ui
 
 import android.content.Context
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -11,8 +15,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
@@ -23,6 +34,7 @@ import kotlin.coroutines.suspendCoroutine
 @Composable
 fun CameraScreen(
 ) {
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -48,6 +60,13 @@ fun CameraScreen(
 
         }
     }
+}
+
+private suspend fun startCamera(
+    context: Context
+) {
+    val cameraProvider = context.getCameraProvider()
+
 }
 
 suspend fun Context.getCameraProvider(): ProcessCameraProvider =
