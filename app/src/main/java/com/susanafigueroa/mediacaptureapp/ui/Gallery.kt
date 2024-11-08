3be2717833426e5942cac94c66dbf7dadde99e7b
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-08 17:31:05 +0100

    show uris from mediaList

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Gallery.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Gallery.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun GalleryScreen(
@@ -22,8 +24,8 @@ fun GalleryScreen(
         columns = GridCells.Adaptive(150.dp),
         modifier = modifier.padding(horizontal = 4.dp)
         ) {
-            items(items = uiState.mediaList, key = { itemMedia -> itemMedia }) {
-
+            items(items = uiState.mediaList, key = { itemMedia -> itemMedia }) {uri ->
+                Text(text = uri.toString())
             }
     }
 }
