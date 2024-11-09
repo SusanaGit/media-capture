4f18e91597b98f208a3d2ff789c25ca02d80e9dd
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-09 18:50:19 +0100

    initialize and show ExoPlayer

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Player.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/Player.kt
@@ -1,14 +1,34 @@
 package com.susanafigueroa.mediacaptureapp.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.PlayerControlView
 
 @Composable
 fun PlayerScreen(
 ) {
-    Column(
-    ) {
-        Text(text = "hola player")
+    val context = LocalContext.current
+
+    val player = remember {
+        ExoPlayer.Builder(context).build().apply {
+            val mediaItem = MediaItem.fromUri("uri")
+            setMediaItem(mediaItem)
+            prepare()
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(factory = { contextPlayer ->
+            PlayerControlView(contextPlayer).apply {
+                this.player = player
+            }
+        })
     }
 }
