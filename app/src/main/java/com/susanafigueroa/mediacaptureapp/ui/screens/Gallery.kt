70e030e920c11de85a4a1fe6ca2ac95588006c49
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-11-12 19:54:22 +0100

    create ShowIcon composable

--- app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/screens/Gallery.kt
+++ app/src/main/java/com/susanafigueroa/mediacaptureapp/ui/screens/Gallery.kt
@@ -3,8 +3,10 @@ package com.susanafigueroa.mediacaptureapp.ui.screens
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -20,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -41,51 +44,73 @@ fun GalleryScreen(
     LazyVerticalGrid(
         columns = GridCells.Adaptive(150.dp),
         modifier = modifier.padding(horizontal = 4.dp)
-        ) {
-            itemsIndexed(uiState.mediaListImages) {index, image ->
+    ) {
+        itemsIndexed(uiState.mediaListImages) { index, image ->
 
-                Box(
-                    modifier = Modifier
-                        .width(200.dp)
-                        .height(300.dp)
-                        .padding(4.dp)
-                        .clickable {
-                            mediaSelectedUri = uiState.mediaList[index]
-                            mediaSelectedUri?.let { uri ->
-                                viewModel.setMediaSelectedUri(uri)
-                                navController.navigate("Player")
-                            }
+            Box(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(300.dp)
+                    .padding(4.dp)
+                    .clickable {
+                        mediaSelectedUri = uiState.mediaList[index]
+                        mediaSelectedUri?.let { uri ->
+                            viewModel.setMediaSelectedUri(uri)
+                            navController.navigate("Player")
                         }
-                ) {
+                    }
+            ) {
 
-                    Image(
-                        bitmap = image,
-                        contentDescription = "image thumbnail",
-                        contentScale = ContentScale.Crop
+                Image(
+                    bitmap = image,
+                    contentDescription = "image thumbnail",
+                    contentScale = ContentScale.Crop
+                )
+
+                if (uiState.mediaList[index].toString().contains("image")) {
+
+                    ShowIcon(
+                        typeMedia = "image",
+                        iconId = R.drawable.baseline_emoji_nature_24
                     )
 
-                    if (uiState.mediaList[index].toString().contains("image")) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_emoji_nature_24),
-                            contentDescription = "photo",
-                            modifier = Modifier
-                                .align(Alignment.BottomEnd)
-                                .padding(5.dp)
-                                .background(Color.LightGray),
-                            tint = Color.DarkGray
-                        )
-                    } else if (uiState.mediaList[index].toString().contains("video")) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_ondemand_video_24),
-                            contentDescription = "video",
-                            modifier = Modifier
-                                .align(Alignment.BottomEnd)
-                                .padding(5.dp)
-                                .background(Color.LightGray),
-                            tint = Color.DarkGray
-                        )
-                    }
+                } else if (uiState.mediaList[index].toString().contains("video")) {
+
+                    ShowIcon(
+                        typeMedia = "image",
+                        iconId = R.drawable.baseline_ondemand_video_24
+                    )
+
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ShowIcon(typeMedia: String, iconId: Int) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = typeMedia,
+            modifier = Modifier
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.LightGray,
+                            Color.Gray,
+                            Color.DarkGray
+                        )
+                    )
+                )
+                .border(1.dp, Color.White),
+            tint = Color.White
+
+        )
     }
 }
